7303f49961ca4722c01584d50ea6bedd73f5cd02
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-11 23:43:46 +0530

    add ProductController to handle product retrieval and category queries

---
+++ src/main/java/com/spring/inventory_service/controller/ProductContoller.java
@@ -0,0 +1,33 @@
+package com.spring.inventory_service.controller;
+
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.inventory_service.entity.Product;
+import com.spring.inventory_service.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductContoller {
+    
+    private final ProductService service;
+
+    @QueryMapping
+    public List<Product> getProducts(){
+        return service.getProducts();
+    }
+
+    @QueryMapping
+    public List<Product> getProductsByCategory(@Argument String category){
+        return service.getProductsByCategory(category);
+    }
+}
