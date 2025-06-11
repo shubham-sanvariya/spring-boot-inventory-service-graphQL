799e3886ef9b27e8bf969032efbef2b364c4bf11
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-11 22:48:14 +0530

    add method to find products by category in ProductRepository

--- src/main/java/com/spring/inventory_service/repo/ProductRepository.java
+++ src/main/java/com/spring/inventory_service/repo/ProductRepository.java
@@ -3,7 +3,10 @@ package com.spring.inventory_service.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.inventory_service.entity.Product;
+import java.util.List;
+
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
     
+    List<Product> findByCategory(String category);
 }
