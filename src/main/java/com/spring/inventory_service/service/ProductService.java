6a8688d92a60acadb604c152b7f59f36a44f3736
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-11 22:46:52 +0530

    implement ProductService to manage product retrieval

---
+++ src/main/java/com/spring/inventory_service/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.spring.inventory_service.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.inventory_service.entity.Product;
+import com.spring.inventory_service.repo.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+}
