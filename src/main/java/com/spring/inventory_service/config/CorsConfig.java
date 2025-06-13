7fa6a80f65316742b4f5c0a4580a077f47381e0b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-13 14:19:47 +0530

    feat: add CORS configuration for GraphQL endpoint

---
+++ src/main/java/com/spring/inventory_service/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.spring.inventory_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsFilter corsFilter() {
+        var source = new UrlBasedCorsConfigurationSource();
+        var config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/graphql", config);
+
+        return new CorsFilter(source);
+    }
+}
